780dd679431109d07e345e9789cd947d97da24e1
ds18b20 <2017603273@qq.com>
2023-05-02 19:47:51 +0800

    include

---
+++ client/include/temp.h
@@ -0,0 +1,20 @@
+/********************************************************************************
+ *      Copyright:  (C) 2023 Hu Ben Yuan<2017603273@qq.com>
+ *                  All rights reserved.
+ *
+ *       Filename:  temp.h
+ *    Description:  This file 
+ *
+ *        Version:  1.0.0(05/02/2023)
+ *         Author:  Hu Ben Yuan <2017603273@qq.com>
+ *      ChangeLog:  1, Release initial version on "05/02/2023 04:05:22 PM"
+ *                 
+ ********************************************************************************/
+
+#ifndef  TEMP_H
+#define  TEMP_H
+
+/* 获取当前温度 */
+int get_temporary(char *temp_buf);
+
+#endif
