79d53cff2fc4975fc53f7270dcfc73d6e6e4c13b
ds18b20 <2017603273@qq.com>
2023-05-07 22:23:40 +0800

    add

---
+++ client/include/get_time.h
@@ -0,0 +1,19 @@
+/********************************************************************************
+ *      Copyright:  (C) 2023 Hu Ben Yuan<2017603273@qq.com>
+ *                  All rights reserved.
+ *
+ *       Filename:  time.h
+ *    Description:  This file 
+ *
+ *        Version:  1.0.0(05/02/2023)
+ *         Author:  Hu Ben Yuan <2017603273@qq.com>
+ *      ChangeLog:  1, Release initial version on "05/02/2023 03:54:29 PM"
+ *                 
+ ********************************************************************************/
+#ifndef   GET_TIME_H_
+#define   GET_TIME_H_
+
+/* 获取当前的时间 */
+int get_time(char *time_buf);
+
+#endif
