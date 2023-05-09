8e357a12182e967bd2f6e88272030d463f861539
ds18b20 <2017603273@qq.com>
2023-05-09 11:25:06 +0800

    new modify

--- client/include/packdata.h
+++ client/include/packdata.h
@@ -2,19 +2,23 @@
  *      Copyright:  (C) 2023 Hu Ben Yuan<2017603273@qq.com>
  *                  All rights reserved.
  *
- *       Filename:  temp.h
+ *       Filename:  packdata.h
  *    Description:  This file 
  *
- *        Version:  1.0.0(05/02/2023)
+ *        Version:  1.0.0(05/05/2023)
  *         Author:  Hu Ben Yuan <2017603273@qq.com>
- *      ChangeLog:  1, Release initial version on "05/02/2023 04:05:22 PM"
+ *      ChangeLog:  1, Release initial version on "05/05/2023 03:47:13 PM"
  *                 
  ********************************************************************************/
 
-#ifndef  TEMP_H
-#define  TEMP_H
+#ifndef PACKDATA_H_
+#define PACKDATA_H_
 
-/* 获取当前温度 */
-int get_temp(char *temp_buf);
+typedef struct packdata_st
+{
+	char    data_time[64];
+	char    data_serial[64];
+	char    data_temp[64];
+} packdata_t;
 
 #endif
